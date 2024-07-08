7c8a24592a29cce2e70845e87b0b82f7c02928ab
Hesriana <jimmy2000117@gmail.com>
2024-07-08 19:12:31 +0800

    Add files via upload

--- string/Untitled-1.cpp
+++ string/Untitled-1.cpp
@@ -1,49 +1,40 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool isSymmetric(TreeNode* root) {
+    string reverseWords(string s) {
+        vector<string> vs;
+        string ans;//答案
+        stack<string> ch;//存字串到stack，然後反著壓出來到vector
+        string temp;//暫存字串
+        temp.clear();
 
-//把樹切成左右兩邊，然後右邊的樹利用dfs跑到底(左邊到底的dfs)，左邊的樹則是利用右邊到底的dfs去跑，不這麼做兩邊不能鏡像去比對        
-        stack<TreeNode*> sr,sl;//left tree sl,right tree sr
-        TreeNode* rnode = root->right;
-        TreeNode* lnode = root->left;
-
-        while(!sr.empty()) sr.pop();
-        while(!sl.empty()) sl.pop();
-
-        while(!sr.empty() || rnode != nullptr || !sl.empty() || lnode != nullptr){
-            while(rnode || lnode){
-                sr.push(rnode);
-                sl.push(lnode);
-
-                if(rnode == nullptr && lnode != nullptr)
-                    return false;
-                else if(rnode != nullptr && lnode == nullptr)
-                    return false;
-                else if(rnode->val != lnode->val)
-                    return false;
-                
-                rnode = rnode->left;//右邊先dfs跑左
-                lnode = lnode->right;//左邊則相反
+        for(int i = 0 ; i < s.size() ; i++){
+            char c = s[i];
+            if(c!=' '){
+                do{
+                    c = s[i];
+                    if(c == ' ')//不這麼做會多執行一次導致存入的字串後面有空格
+                        break;
+                    temp.push_back(c);
+                    i++;
+                    
+                }while(c!=' ' && s[i]!=NULL);
+                i--;//把多跑的一個字元扣回來
+                ch.push(temp);
+                temp.clear();
             }
-            rnode = sr.top();
-            lnode = sl.top();
-            sr.pop();
-            sl.pop();
-            rnode = rnode->right;
-            lnode = lnode->left;
+        }
+
+        while(!ch.empty()){
+            vs.push_back(ch.top());
+            ch.pop();
         }
         
-        return true;
+        for(int i = 0 ; i < vs.size() ; i++){
+            ans += vs[i];//字串連接方法
+            if(i != vs.size() - 1)
+                ans.push_back(' ');//將字元放入字串的方法
+        }
+
+        return ans;
     }
 };
